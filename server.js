7e24d0a8b2826cd3cf308e5397b7037e874b8763
Apratim Nath <nathapratim@gmail.com>
2020-11-25 22:58:00 +0530

    Update Node Server File

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const forceSSL = function () {
+	return function (req, res, next) {
+		if (req.headers['x-forwarded-proto'] !== 'https') {
+			return res.redirect(
+				['https://', req.get('Host'), req.url].join('')
+			);
+		}
+		next();
+	}
+}
+app.use(express.static(__dirname + '/dist/mf-analysis-dashboard'));
 
-app.use(express.static(__dirname + '/dist'))
+app.get('/*', function (req, res) {
+	res.sendFile(path.join(__dirname + '/dist/mf-analysis-dashboard/index.html'));
+});
 
 app.listen(process.env.PORT || 9800);
-
-app.get('/*', function(req, res){
-	res.sendFile(path.join(__dirname + '/dist/index.html'));
-})
 
 console.log(`Mutual Fund Aanalysis Dashboard listening on port ${port}!`);
