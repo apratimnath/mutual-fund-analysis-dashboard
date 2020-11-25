01396f2b6245a10ea6f823c99a672a9ae0bc29a3
Apratim Nath <nathapratim@gmail.com>
2020-11-25 22:41:15 +0530

    Updated Project Structure for Heroku

--- server.js
+++ server.js
@@ -1,27 +1,13 @@
-'use strict';
+const express = require('express');
+const app = express();
+const path = require('path');
 
-const express = require('express')
-const helmet = require('helmet')
-const frameguard = require('frameguard')
-const app = express()
-const port = process.env.PORT || 9800
-const http = require('http')
+app.use(express.static(__dirname + '/dist'))
 
-var path_1 = require('path')
-var dist_folder = path_1.join(process.cwd(), 'mf-analysis-dashboard/dist/mf-analysis-dashboard');
+app.listen(process.env.PORT || 9800);
 
-app.use(helmet());
+app.get('/*', function(req, res){
+	res.sendFile(path.join(__dirname + '/dist/index.html'));
+})
 
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
-
-app.set('views', path_1.join(dist_folder, ''));
-app.get('*.*', express.static(path_1.join(dist_folder, ''), {
-}));
-
-app.get('*', function(req, res, next){
-	res.render('index.html', { req: req});
-});
-
-var server = http.createServer(app).listen(port, () => console.log(`Mutual Fund Aanalysis Dashboard listening on port ${port}!`));
-	
+console.log(`Mutual Fund Aanalysis Dashboard listening on port ${port}!`);
